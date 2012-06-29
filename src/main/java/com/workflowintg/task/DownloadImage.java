fa87343bf3ce226225365e391e01a13a4943e221
Emilio Alvarez <emiliolalvarez@gmail.com>
2012-06-29 01:47:48 -0300

    Apache http client

--- src/main/java/com/workflowintg/task/DownloadImage.java
+++ src/main/java/com/workflowintg/task/DownloadImage.java
@@ -5,10 +5,13 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.URI;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.params.DefaultedHttpParams;
 import org.apache.log4j.Logger;
-import org.xlightweb.GetRequest;
-import org.xlightweb.IHttpRequest;
-import org.xlightweb.IHttpResponse;
 import org.xlightweb.client.MyHttpClient;
 
 import com.myworkflow.task.TaskAsync;
@@ -37,34 +40,48 @@ public class DownloadImage extends TaskCallable{
 		try {
 			uri = new URI(url);
 			fileName = "/tmp/"+((AdWorkflow)t.getWorkflow()).getPartner()+"-"+uri.getPath().replace("/", "");
-			
 		    LOGGER.info("Image ["+url+"] downloaded");
-			
-			IHttpRequest req = new GetRequest(url);
-			c = HttpConnectionPool.getConnectionPool(uri.getHost(), uri.getPort()).take();
-			IHttpResponse resp = c.call(req);
-			File f = new File(fileName);
-			if(resp.hasBody()){
-				f.createNewFile();
-				FileOutputStream fos = new FileOutputStream(f);
-				InputStream is = resp.getBody().toInputStream();
-				byte[] buffer = new byte[128];
-		        int len = 0;
-		        while ((len = is.read(buffer)) > 0) {
-		           fos.write(buffer, 0, len);
-		        }
-		        fos.close();
-		        is.close();
-		        LOGGER.info("Image ["+url+"] downloaded");
-			}
-			
+		    DefaultHttpClient client = new DefaultHttpClient();
+		    client.setHttpRequestRetryHandler(new DefaultHttpRequestRetryHandler(3, true));
+		    HttpGet httpget = new HttpGet(url);
+		    HttpResponse response = client.execute(httpget);
+		    File f = new File(fileName);
+		    f.createNewFile();
+			FileOutputStream fos = new FileOutputStream(f);
+			InputStream is =  response.getEntity().getContent();
+			byte[] buffer = new byte[128];
+	        int len = 0;
+	        while ((len = is.read(buffer)) > 0) {
+	           fos.write(buffer, 0, len);
+	        }
+	        fos.close();
+	        is.close();
+	        LOGGER.info("Image ["+url+"] downloaded");
+			//IHttpRequest req = new GetRequest(url);
+			//c = HttpConnectionPool.getConnectionPool(uri.getHost(), uri.getPort()).take();
+//			IHttpResponse resp = c.call(req);
+//			File f = new File(fileName);
+//			if(resp.hasBody()){
+//				f.createNewFile();
+//				FileOutputStream fos = new FileOutputStream(f);
+//				InputStream is = resp.getBody().toInputStream();
+//				byte[] buffer = new byte[128];
+//		        int len = 0;
+//		        while ((len = is.read(buffer)) > 0) {
+//		           fos.write(buffer, 0, len);
+//		        }
+//		        fos.close();
+//		        is.close();
+//		        LOGGER.info("Image ["+url+"] downloaded");
+//			}
+			//HttpConnectionPool.releaseConnection(uri.getHost(),uri.getPort(), c);
 		} 
 		catch(Exception e){
 			LOGGER.error("Image ["+url+"] failed "+"\n"+fileName);
 			e.printStackTrace();
 		}
 		
-		HttpConnectionPool.releaseConnection(uri.getHost(),uri.getPort(), c);
+		
 		
 		
 		return new TaskAsyncResult("success",t,this);
